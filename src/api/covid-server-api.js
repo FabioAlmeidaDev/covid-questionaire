97b1fe1fa585bb45cd50fa1624cd9100ed74cdfe
fabio almeida <fabioalmeida@ge.com>
2020-06-27 03:01:34 -0400

    make it better

--- src/api/covid-server-api.js
+++ src/api/covid-server-api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://covid-waiver-server.herokuapp.com',
   headers: {
     // Authorization: 'Bearer JUZpWvW06EaLF1DYq7O8pb4lN4D5nMoo3GpQ3tjODiNa34ZCu_1uyUUtLiQjUPEJ_rD0tSsAsQfQWZTL6Fey0WlrUraSJet5t4m81bLHsC16d7NE6pRHMQtJreuxXXYx'
   }
