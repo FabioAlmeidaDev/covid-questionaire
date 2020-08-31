8e71b13843099613435aebd6e8601e4ea209f9af
Fabio Almeida <fabioalmeida@Fabios-MBP.home>
2020-08-31 19:45:44 -0400

    aded loading screen

--- src/pages/components/Searchbar.tsx
+++ src/pages/components/Searchbar.tsx
@@ -6,25 +6,47 @@ import { withConditionalRender } from "../../enhancers/withConditionalRender";
 import capitalize from "../functions/capitalize";
 import Refresh from "@material-ui/icons/Refresh";
 import { Container, Paper as P, Button as MUIButton } from "@material-ui/core";
+import { css } from "@emotion/core";
+import BounceLoader from "react-spinners/BounceLoader";
 
 export const Search = withConditionalRender((props: any) => {
   const [inputValue, setInputValue] = React.useState("");
   const [athleteList, setAthleteList] = React.useState([]);
+  const [spinner, setSpinner] = React.useState(false);
 
   React.useEffect(() => {
     updateAthletesList();
   }, []);
 
   const updateAthletesList = () => {
-    getUserList().then((data) => {
-      setAthleteList(data);
-    });
+    setSpinner(true);
+    getUserList().then(
+      (data) => {
+        setAthleteList(data);
+        setSpinner(false);
+      },
+      () => {
+        setSpinner(false);
+      }
+    );
   };
   const Button = withConditionalRender(MUIButton);
+
+  // spinner
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `;
 
   const { value, setValue } = props;
   return (
     <div className="row search-row">
+      {spinner ? <div className="spinner-backdrop" /> : ""}
+      <div className="sweet-loading">
+        <BounceLoader css={override} size={150} color={"#123abc"} loading={spinner} />
+        {spinner ? <div className="loading-athletes-list">Loading athlete's list</div> : ""}
+      </div>
       <Autocomplete
         value={value ? value : null}
         onChange={(event, newValue) => {
